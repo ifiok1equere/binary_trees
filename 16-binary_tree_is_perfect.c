bdaf894635760a4d33cd98333cd018553573ebae
ifiok1equere <equereifiok1@gmail.com>
2023-12-01 01:50:26 +0100

    task 16: Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,13 +12,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height != right_height)
-		return 0;
+		return (0);
 
 
 	return (binary_tree_is_perfect(tree->left) &&
